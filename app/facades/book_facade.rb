1c728cc1c0bf18213763a4871df33f0be630e5db
Maximilian Wagner <maw3350@gmail.com>
2022-06-13 12:25:12 -0400

    feat: add poros for book facade

--- app/facades/book_facade.rb
+++ app/facades/book_facade.rb
@@ -5,15 +5,13 @@ class BookFacade
       weather = ForecastService.get_weather(coords[:lat],coords[:lng])
       current  = CurrentAbridged.new(weather[:current])
 
-
+      #attributes for poro start here
       destination = location
       forecast = current
       total_books_found = BookService.get_books(location)[:numFound]
       books = BookService.get_books(location)[:docs].each do |book|
         Book.new(book)
       end
-
-
 
       BookData.new(destination, forecast, total_books_found, books)
     end

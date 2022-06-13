03e3829746b9aec08a579b90c146c8ead38a28dd
Maximilian Wagner <maw3350@gmail.com>
2022-06-13 01:45:23 -0400

    feat: add daily and hourly poros

---
+++ app/poros/hourly.rb
@@ -0,0 +1,13 @@
+class Hourly
+  attr_reader :time,
+              :temperature,
+              :conditions,
+              :icon
+
+  def initialize(data)
+    @time = Time.at(data[:dt]).strftime("%k:%M")
+    @temperature = data[:temp]
+    @conditions = data[:weather][0][:description]
+    @icon = data[:weather][0][:icon]
+  end
+end

f0ba941110e65a4ca7b4ed870caecf64960864af
Maximilian Wagner <maw3350@gmail.com>
2022-06-14 22:14:24 -0400

    feat: add session functionality

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
     def create
         if params[:password] == params[:password_confirmation] && User.find_by_email(params[:email]).nil?
             User.create!(email: params[:email], password: params[:password])
-            render json: UsersSerializer.new(User.last)
+            render json: UserSerializer.new(User.last)
         elsif params[:password] != params[:password_confirmation]
             render json: {error: 'passwords do not match', status: 400}
         elsif User.find_by_email(params[:email])
